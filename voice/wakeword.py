ba628f4995dd26b0f368218bbdccae85c8714cc6
Rajveer Rajput <rajveerhada09@gmail.com>
2026-06-29 20:00:38 +0530

    Reorganize project architecture

---
+++ voice/wakeword.py
@@ -0,0 +1,17 @@
+"""
+============================================================
+PROJECT : JARVIS MARK 5
+
+FILE    : wakeword.py
+
+PATH    : voice\wakeword.py
+
+PURPOSE :
+Module description pending.
+
+LAST UPDATED :
+2026-06-29
+
+============================================================
+"""
+
